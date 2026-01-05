2fe4b624f00d309518bf0f1750e04dee14630032
Dipendra Mahato <dipendram891@gmail.com>
2026-01-05 14:33:21 +0000

    creat the suitable bucket in the firebase and connet it so the this prob

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:231722190452:web:e7e1ff5d19fbc91871b321",
   "apiKey": "AIzaSyBHyHUEKnwm9PJdmkGLMSeUobDAMV3ubUI",
   "authDomain": "studio-4897018311-c7624.firebaseapp.com",
+  "storageBucket": "studio-4897018311-c7624.appspot.com",
   "measurementId": "",
   "messagingSenderId": "231722190452"
 };

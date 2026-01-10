352bb6243c076025c3ec1669dc0b10eb1f137ac6
Dipendra Mahato <dipendram891@gmail.com>
2026-01-10 03:56:59 +0000

    make this one (Sri Ramakrishna Hospital) colourfull and anniminated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,6 +97,11 @@ export default {
         'slide-up': {
             from: { transform: 'translateY(20px)', opacity: '0'},
             to: { transform: 'translateY(0)', opacity: '1'},
+        },
+        'text-gradient': {
+          'to': {
+            'background-position': '200% center',
+          }
         }
       },
       animation: {
@@ -104,6 +109,7 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in': 'fade-in 0.5s ease-out',
         'slide-up': 'slide-up 0.5s ease-out forwards',
+        'text-gradient': 'text-gradient 3s linear infinite',
       },
       boxShadow: {
         'soft': '0 4px 12px 0 rgba(0, 0, 0, 0.05)',

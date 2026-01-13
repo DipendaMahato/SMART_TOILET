c48984a3178023bfb756d766b9c080873d67809a
Dipendra Mahato <dipendram891@gmail.com>
2026-01-13 03:25:04 +0000

    still  available only form the date of birt just put the  calander for s

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -19,7 +19,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      weekStartsOn={0} // Start week on Sunday
+      weekStartsOn={0}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
